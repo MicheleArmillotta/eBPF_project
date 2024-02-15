9d8b5ebb2859b2780c7ee5fce26b2f19602af50c
Michele Armillotta <102028613+MicheleArmillotta@users.noreply.github.com>
2024-02-15 15:34:25 +0100

    Update project.bpf.c

--- project.bpf.c
+++ project.bpf.c
@@ -1,98 +1,70 @@
-#include <linux/types.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_core_read.h>
-#include <linux/sched.h>
-#include <linux/string.h>
-#include <linux/fs.h>
+#include <vmlinux.h>
 #include <errno.h>
-#include <bpf/bpf_tracing.h>
-#include <linux/ptrace.h> 
+#include <bpf/bpf_helpers.h>
+#include <asm/rwonce.h>
+#include <linux/jhash.h>
+
+
+
+
+
 
 
 #define MAX_PATH_LEN 100
-#define MAX_PROCESSES 10
-#define MAX_FILES_PER_KEY 10
-#define MAX_ENTRIES 6
-#define MAX_KEY 10
+#define MAX_ENTRIES 5
 
-struct file_and_process_list {
-    char file_paths[MAX_FILES_PER_KEY][MAX_PATH_LEN];
-    char process_paths[MAX_PROCESSES][MAX_PATH_LEN];
-};
+
+//OPEN FILES AND PROCESSES
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, MAX_ENTRIES);
-    __type(key, sizeof(char[MAX_KEY]));
-	__type(value, sizeof(struct file_and_process_list));
-} file_and_process_map SEC(".maps") ;
+	__uint(max_entries, MAX_ENTRIES); //max 5 file da proteggere per questa syscall
+    __type(key, __u32);
+	__type(value, __u32);
+} OPEN_FILES_MAP SEC(".maps") ;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES); //max 5 proc esenti per questa syscall
+    __type(key, __u32);
+	__type(value, __u32);
+} OPEN_PROC_MAP SEC(".maps") ;
 
 
 
-SEC("kprobe/vfs_open")
-int kprobe_vfs_open(struct pt_regs *ctx)
+SEC("tracepoint/syscalls/sys_enter_openat")
+int tracepoint__syscalls__sys_enter_openat(struct trace_event_raw_sys_enter *ctx)
 {
     int flag_process=1; //1 vuol dire che il processo corrente non è un processo che puo fare l'azione
     int flag_path=0; //1 vuol dire che il path del file è il path non consentito
-    char current_event[MAX_KEY] = "open"; // Esempio di evento
-    struct file_and_process_list *list; // Elenco dei percorsi dei file e dei processi consentiti per la chiave corrente
-
-    // Ottieni l'elenco dei percorsi dei file e dei processi consentiti dalla mappa BPF
-    list = bpf_map_lookup_elem(&file_and_process_map, &current_event);
-
-    if(list){
+    int *value_file;
+    int *value_proc;
+    char comm[MAX_PATH_LEN];
+    char filename[MAX_PATH_LEN];
+    __u32 hash_file;
+    __u32 hash_proc;
         
-        char comm[MAX_PATH_LEN];
-        char filename[MAX_PATH_LEN];
-
         //otteniamo percorso del file
-        bpf_probe_read_user(filename, sizeof(filename), (void *)PT_REGS_PARM1(ctx));
+    bpf_probe_read_user(filename, sizeof(filename), (char*)ctx->args[1]);
         //otteniamo nome del processo
-        bpf_get_current_comm(comm, sizeof(comm));
+    bpf_get_current_comm(comm, sizeof(comm));
 
+        // Ora puoi fare ciò che vuoi con il nome del file e il nome del processo
+    hash_proc= jhash(comm, MAX_PATH_LEN, 0);
+    hash_file= jhash(filename, MAX_PATH_LEN, 0);
+
+    value_file = bpf_map_lookup_elem(&OPEN_FILES_MAP,&hash_file);
+
+    value_proc = bpf_map_lookup_elem(&OPEN_PROC_MAP,&hash_proc); 
+
+    //bpf_printk("File aperto: %d, Processo: %d\n", *value_file,*value_proc); 
+    
+    if(value_file != NULL && *value_file > 0)
+        flag_path=1;
+    
+    if(value_proc != NULL && *value_proc > 0)
+        flag_process=0;
         
-        bpf_printk("File aperto: %s, Processo: %s\n", filename, comm);
-
-        
-
-        
-        for (int i = 0; i < MAX_PROCESSES; i++) {
-            int j;
-            for (j = 0; j < MAX_PATH_LEN; j++) {
-                if (comm[j] != list->process_paths[i][j]) {
-                    break;
-                }
-                if (comm[j] == '\0' && list->process_paths[i][j] == '\0') {
-                    flag_process = 0;
-                    break;
-                }
-            }
-            if (flag_process == 0) {
-                break;
-            }
-        }
-
-            if (flag_process == 1) {
-                for (int i = 0; i < MAX_FILES_PER_KEY; i++) {
-                    int j;
-                    for (j = 0; j < MAX_PATH_LEN; j++) {
-                        if (filename[j] != list->file_paths[i][j]) {
-                            break;
-                        }
-                        if (filename[j] == '\0' && list->file_paths[i][j] == '\0') {
-                            flag_path = 1;
-                            break;
-                        }
-                    }
-                    if (flag_path == 1) {
-                        break;
-                    }
-                }
-            }
-        }
 
 
     if(flag_path==1 && flag_process==1){
@@ -101,7 +73,7 @@ int kprobe_vfs_open(struct pt_regs *ctx)
         
     }
     else{
-        bpf_printk("un accesso riuscito");
+        
         return 0;
     }
 }
